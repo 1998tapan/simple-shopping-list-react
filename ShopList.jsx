b334dcd6dae0eb4c44030ed0a86c1f8f590e7391
Tapan Dabrai <1998tapan@gmail.com>
2024-07-13 12:33:22 +0530

    list item sub comp added

--- ShopList.jsx
+++ ShopList.jsx
@@ -1,10 +1,25 @@
+import ListItem from "./src/ListItem"
+
 export default function ShopList({ items }) {
     return (
         <div className="shop-container">
             <h1>Shopping List React</h1>
             <ul>
-                {items.map(item => (<li>{item.name} - {item.quantity}</li>))}
+                {items.map(item => (
+                    <ListItem
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        completed={item.completed}
+                    />
+                ))}
             </ul>
         </div>
     )
 }
+
+
+{/* <ListItem
+    key={item.id}
+    {...item}
+/> */}
---
+++ src/ListItem.jsx
@@ -0,0 +1,11 @@
+export default function ListItem({ name, quantity, completed }) {
+    const styles = {
+        color: completed ? "grey" : "red"
+        , textDecoration: completed ? "line-through" : "none"
+    }
+    return (
+        <li style={styles}>
+            {name} - {quantity}
+        </li>
+    )
+}
